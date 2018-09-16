bb5e77dacaa2394b1e98f41a9c5dbf1f59b67ce9
kujbika <43244473+kujbika@users.noreply.github.com>
2018-09-16 13:50:41 +0200

    Repaired version of the homework

---
+++ header.R
@@ -0,0 +1,47 @@
+#this file contains all the packages and functions that can be helpful for the work.
+using<-function(...) {
+  libs<-unlist(list(...))
+  req<-unlist(lapply(libs,require,character.only=TRUE))
+  need<-libs[req==FALSE]
+  if(length(need)>0){ 
+    install.packages(need)
+    lapply(need,require,character.only=TRUE)
+  }
+}
+libraries=c("randomForest","corrplot","stats","ggExtra","ggalt","timeSeries","ROCR","mgcv","e1071","data.table","ggplot2","ggthemes","mice","robustHD","caret","lattice","plotROC", "OptimalCutpoints", "pROC", "plotly")
+using(libraries)
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
--- overfit_exercise.R
+++ overfit_exercise.R
@@ -1,22 +1,44 @@
-x=data.table(x=seq(0,2.5,0.25))
-u=data.table(u=rnorm(11,0,5))
-data=data.table(x,u,reality=5*x^3,z=5*x^3+u)
-names(data)=c("x","u","reality","z")
-View(data)
-p1=ggplot(data=data,aes(x=x,y=z))+geom_point()
-p1
-average=mean(data$z)
-d0=p1+ggtitle("0 degree")+geom_hline(yintercept = average, col="blue")+annotate('text',x=0.35,y=average+2, label=paste0("y=",round(average,3)))
-d0
-m1=lm(data=data,z~x)
-ess1=sum((m1$fitted.values-mean(data$z)))
-ess1
-ess_counter=function(k){
-  while(k>0){
-    data$q=x^k
-    m1=update(m1,.~.+q, data=data)
-    k=k-1
-  }
-  ess=sum((m1$fitted.values-mean(data$z)))
-  return(summary(m1))
+setwd("C:/Users/User/Documents/GitHub/learning_repo")
+source("header.R")
+data_creater=function(p){
+  #p=number of points
+  x=seq(0,2,2/(p-1))
+  d=data.table(x,z=2*x^3+rnorm(p,0,3))
+  #p=ggplot(d,aes(x=x,y=z))+geom_point()+ggtitle("The raw data")
+  #print(p)
+  return(d)
 }
+ess_counter=function(k,d){
+  #k is the number of degrees, d is the datatable
+  data_fun=data.table(z=d$z)
+  while(k>0){
+  data_fun=data.table(cbind(data_fun,d$x^k))
+  k=k-1
+  }
+  for (i in 2:ncol(data_fun)) names(data_fun)[i]=paste(ncol(data_fun)-i+1)
+  m=lm(data=data_fun, z~.)
+  ess=sum((m$fitted.values-data_fun$z)^2)
+  ls=list(data_fun,ess)
+  return(ls)
+}
+simulate_ess=function(m){
+  #m is the number of rows of the data table
+  q=data_creater(m)
+  ess_sol=data.table(degree=1:m,ess=rep(0,m))
+  for (i in 1:m) ess_sol$ess[i]=ess_counter(i,q)[[2]]
+  return(ess_sol)
+}
+
+##lets see an example. 
+t=ess_counter(5,data_creater(30))
+t[[1]]
+t[[2]]
+simulate_ess(30)
+##
+
+q=data.table(degree=1:30)
+for (i in 1:100) q=cbind(q,data.table(simulate_ess(30)[,2]))
+q$mean=apply(q[,-1],1,mean)
+q$sd=apply(q[,-1],1,sd)
+ess_solution=data.table(degree=1:30,ess=q$mean,stand.error=q$sd)
+ggplot(ess_solution,aes(x=degree,y=ess))+geom_point()+ggtitle("The ess converges to zero as the degree increases. Overfitting is detected.")
